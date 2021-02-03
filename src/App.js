01efa514833c5b09d62409821bcdd7b1b3944660
Alexander Kholodnyak <kholodnyak.a@gmail.com>
2021-02-03 14:04:36 +0200

    4-1 fin

--- src/App.js
+++ src/App.js
@@ -1,62 +1,84 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Section from './components/Section';
 import FeedbackOptions from './components/FeedbackOptions';
 import Statistics from './components/Statistics';
 import Notification from './components/NotificationMessage';
-//import './App.css';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+function App() {
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // };
 
-  onFeedbackRefresh = ({ target }) => {
-    const type = target.dataset.action;
-    this.setState(prevState => {
-      return { [type]: prevState[type] + 1 };
-    });
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // onFeedbackRefresh = ({ target }) => {
+  //   const type = target.dataset.action;
+  //   this.setState(prevState => {
+  //     return { [type]: prevState[type] + 1 };
+  //   });
+  // };
+
+  const options = ['good', 'neutral', 'bad'];
+
+  const onFeedbackRefresh = option => {
+    switch (option) {
+      case 'good':
+        setGood(good + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+
+      case 'bad':
+        setBad(bad + 1);
+        break;
+
+      default:
+        return;
+    }
   };
-  onTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const onTotalFeedback = () => {
+    // const { good, neutral, bad } = this.state;
     return good + neutral + bad;
   };
 
-  onPositive = () => {
-    return this.onTotalFeedback()
-      ? Math.round((this.state.good / this.onTotalFeedback()) * 100)
-      : 0;
+  const onPositive = () => {
+    return onTotalFeedback() ? Math.round((good / onTotalFeedback()) * 100) : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = ['good', 'neutral', 'bad'];
+  // render() {
+  //   const { good, neutral, bad } = this.state;
+  // const options = ['good', 'neutral', 'bad'];
 
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onFeedback={this.onFeedbackRefresh}
-          ></FeedbackOptions>
-        </Section>
-        <Section title="Statistics">
-          {this.onTotalFeedback() ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.onTotalFeedback()}
-              positivePercentage={this.onPositive()}
-            />
-          ) : (
-            <Notification message="No feedback given">)</Notification>
-          )}
-        </Section>
-      </>
-    );
-  }
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onFeedback={onFeedbackRefresh}
+        ></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        {onTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={onTotalFeedback()}
+            positivePercentage={onPositive()}
+          />
+        ) : (
+          <Notification message="No feedback given"></Notification>
+        )}
+      </Section>
+    </>
+  );
+  // }
 }
 
 export default App;
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ options, onFeedback }) => (
-  <div>
-    {options.map(option => (
-      <button
-        type="button"
-        data-action={option}
-        onClick={onFeedback}
-        key={option}
-        className={s.button}
-      >
-        {option}
-      </button>
-    ))}
-  </div>
-);
+function FeedbackOptions({ options, onFeedback }) {
+  return options.map(option => (
+    <button
+      type="button"
+      data-action={options}
+      // onClick={onFeedback}
+      onClick={() => onFeedback(option)}
+      key={option}
+      className={s.button}
+    >
+      {option}
+    </button>
+  ));
+}
 
 FeedbackOptions.propTypes = {
   options: PropTypes.array,
